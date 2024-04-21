ea9e7fc9b5743cec41319f2a275019ed056a31d3
Valeria Jara <valeria.jaraarenas@gmail.com>
2024-04-21 19:39:38 -0400

    adding logo and Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,20 @@ import React from 'react'
 import * as S from "./Navbar.styles";
 
 const Navbar = () => {
-  return <S.ContainerNavbar>Nuestro container navbar</S.ContainerNavbar>;
+  return <S.ContainerNavbar>
+    <S.ContainerLogo>
+        <S.imageLogo src="/img/Logo.ico" alt="Logo Restaurant App" />
+        <div>
+            <h1>Dolce Rosa</h1>
+            <h2>Exquisita gatronomía y ambiente</h2>
+        </div>
+    </S.ContainerLogo>
+    <S.ContainerItems>
+        <p>About me</p>
+        <p>Projects</p>
+        <S.button>Contact me</S.button>
+    </S.ContainerItems>
+</S.ContainerNavbar>;
 };
 
 export default Navbar;
--- src/components/Navbar/Navbar.styles.js
+++ src/components/Navbar/Navbar.styles.js
@@ -3,8 +3,70 @@ import { colors } from "../../styles/colors";
 
 export const ContainerNavbar = styled.div`
 
-height : 100px;
-background-color: ${colors.BACKGROUND_GRADIENT_PINK};
+max-width: 100vw;
+height: 100px;
+box-shadow: ${colors.BOX_SHADOW};
+display: flex;
+align-items: center;
+justify-content: space-between;
+padding: 0 20px;
+background: ${colors.TEXT_WHITE};
+`;
+
+export const ContainerLogo = styled.div`
+display: flex;
+align-items: center;
 
 
+h1, h2 {
+font-weight: 700;
+color: ${colors.TEXT_BLACK};
+margin: 0;
+}
+
+h1 {
+font-size: 2rem;
+}
+
+h2 {
+background: linear-gradient(135deg, #aa5f50 0%, #e68571 100%);
+background-clip: text;
+-webkit-background-clip: text;
+-webkit-text-fill-color: transparent;}
+`;
+
+export const imageLogo = styled.img`
+width: 90px;
+padding-right: 20px;
+`;
+
+export const ContainerItems = styled.div`
+display: flex;
+align-items: center;
+
+p {
+    cursor: pointer;
+    padding: 0 13px;
+    text-decoration: none;
+    color: ${colors.TEXT_BLACK};
+    font-weight: 500;
+}
+`;
+
+export const button = styled.button`
+color: ${colors.TEXT_WHITE};
+box-shadow: ${colors.BOX_SHADOW_BUTTON};
+background: linear-gradient(135deg, #aa5f50 0%, #e68571 100%);
+padding: 12px 20px;
+border-radius: 999px;
+border: none;
+margin: 10px 0;
+font-size: 16px;
+font-weight: 700;
+cursor: pointer;
+
+@media (max-width: 760px){  
+width: 50%;
+margin: 5px 10px 0 10px;
+}
 `;
